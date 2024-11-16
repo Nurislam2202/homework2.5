e6c8d87b349b354efaf5d9661c1f960f135bdd3f
Nurislam2202 <nurislamuraimov2202@gmail.com>
2024-11-16 14:25:34 +0600

    'homework6'

--- home.py
+++ home.py
@@ -31,6 +31,7 @@ class CheckingAccount(BankAccount):
         else:
             print("Overdraft limit exceeded")
 
+
 class Bank:
     def __init__(self, name):
         self.name = name
@@ -51,17 +52,37 @@ class Bank:
         print(f"Total funds in the bank: {total}")
         return total
 
+
+class SavingsAccount(BankAccount):
+    def __init__(self, account_number, owner, interest_rate, balance=0):
+        super().__init__(account_number, owner, balance)
+        self.interest_rate = interest_rate
+
+    def add_interest(self, amount):
+        if amount < 0 or amount > 100:
+            print('enter numbers only from 0 to 100')
+        elif amount == 0:
+            print(f'balance: {self.balance}')
+        else:
+            self.interest_rate = amount / 100
+            self.balance += self.balance * self.interest_rate
+            print(f'interest rate: {amount}%. New balance: {self.balance}')
+
+
 # Создаем счета
 checking = CheckingAccount("CA54321", "Bob", 500, 200)
+saving = SavingsAccount('NU59669', 'Nurislam', 50, 500)
 
 # Создаем банк и добавляем счета
 bank = Bank("MyBank")
 bank.add_account(checking)
+bank.add_account(saving)
 
 # Действия с счетами
-
 checking.withdraw(600)
 checking.deposit(300)
+saving.add_interest(0)
+saving.deposit(500)
 
 # Выводим список счетов и общую сумму денег в банке
 bank.list_accounts()
